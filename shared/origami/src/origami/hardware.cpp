d048f68e5001760413af9b68845f86401fe8f390
aliry95amd <ayazdani@amd.com>
2025-12-19 00:05:31 +0000

    Update weights for CMS kernels

--- shared/origami/src/origami/hardware.cpp
+++ shared/origami/src/origami/hardware.cpp
@@ -18,7 +18,26 @@ const std::unordered_map<hardware_t::architecture_t,
     hardware_t::CMS_MAP = {
       {hardware_t::architecture_t::gfx950,
         {
-          {CMS_kernel(data_type_t::BFloat16, transpose_t::T, transpose_t::N, 256, 256, 64), 1. / 1.},
+          // BF16
+          // NT
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::N, transpose_t::T, 160, 256, 64), 1. / 1.20},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::N, transpose_t::T, 192, 256, 64), 1. / 1.10},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::N, transpose_t::T, 208, 256, 64), 1. / 1.20},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::N, transpose_t::T, 256, 160, 64), 1. / 1.20},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::N, transpose_t::T, 256, 192, 64), 1. / 1.20},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::N, transpose_t::T, 256, 256, 64), 1. / 1.15},
+          // NN
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::N, transpose_t::N, 160, 256, 64), 1. / 1.10},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::N, transpose_t::N, 208, 256, 64), 1. / 1.10},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::N, transpose_t::N, 256, 192, 64), 1. / 1.00},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::N, transpose_t::N, 256, 256, 64), 1. / 1.05},
+          // TN
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::T, transpose_t::N, 160, 256, 64), 1. / 1.10},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::T, transpose_t::N, 192, 256, 64), 1. / 1.05},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::T, transpose_t::N, 256,  96, 64), 1. / 1.10},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::T, transpose_t::N, 256, 192, 64), 1. / 1.10},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::T, transpose_t::N, 256, 224, 64), 1. / 1.05},
+          {CMS_kernel(data_type_t::BFloat16, transpose_t::T, transpose_t::N, 256, 256, 64), 1. / 1.05},
         }
       },
     };
