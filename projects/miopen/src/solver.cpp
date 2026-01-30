8ee924978769346f4a81735dc228a8b1291bf289
Vasilii Maksimov <vasilii.maksimov@amd.com>
2026-01-30 06:37:04 -0500

    replace boost::adaptors::transformed()

--- projects/miopen/src/solver.cpp
+++ projects/miopen/src/solver.cpp
@@ -1,28 +1,5 @@
-/*******************************************************************************
- *
- * MIT License
- *
- * Copyright (c) 2026 Advanced Micro Devices, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- *******************************************************************************/
+// Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
+// SPDX-License-Identifier: MIT
 
 #include <miopen/activ/solvers.hpp>
 #include <miopen/adam/solvers.hpp>
@@ -53,8 +30,8 @@
 #include <miopen/any_solver.hpp>
 #include <miopen/timer.hpp>
 
-#include <boost/range/adaptor/transformed.hpp>
 #include <ostream>
+#include <ranges>
 
 namespace miopen {
 namespace solver {
@@ -120,7 +97,7 @@ void PrecompileSolutions(const Handle& h,
 std::ostream& operator<<(std::ostream& os, const ConvSolution& s)
 {
     auto strings =
-        s.construction_params | boost::adaptors::transformed([](auto k) { return k.kernel_name; });
+        s.construction_params | std::views::transform([](auto k) { return k.kernel_name; });
     os << s.solver_id << ": " << JoinStrings(strings, "/");
     return os;
 }
