9816873fe8c9a7764ea8f922c26517dc861b5bb5
Sander Bos <sander@streamhpc.com>
2026-02-06 22:16:11 +0000

    chore(primbench): apply clang-format

--- shared/primbench/primbench.hpp
+++ shared/primbench/primbench.hpp
@@ -1741,9 +1741,9 @@ private:
                                    bool             noise_timeout,
                                    double           noise_percent)
     {
-        m_csv_out << index << ",\"" << name << "\""
-                  << "," << bytes_per_sec << "," << bytes_per_sec / (1024.0 * 1024.0 * 1024.0)
-                  << "," << items_per_sec << "," << noise_timeout << "," << noise_percent << "\n"
+        m_csv_out << index << ",\"" << name << "\"" << "," << bytes_per_sec << ","
+                  << bytes_per_sec / (1024.0 * 1024.0 * 1024.0) << "," << items_per_sec << ","
+                  << noise_timeout << "," << noise_percent << "\n"
                   << std::flush;
     }
 
