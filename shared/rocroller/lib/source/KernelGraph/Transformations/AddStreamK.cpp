256bd0a4acf4683e22516b7142ae6aa35f8e9bb0
Caio Salvador Rohwedder <Caio.SalvadorRohwedder@amd.com>
2026-02-06 19:31:22 -0500

    Remove enableDivideBy

--- shared/rocroller/lib/source/KernelGraph/Transformations/AddStreamK.cpp
+++ shared/rocroller/lib/source/KernelGraph/Transformations/AddStreamK.cpp
@@ -1029,7 +1029,6 @@ namespace rocRoller
             for(auto d : loopInfo.dimensionIndices)
                 numTotalTiles = numTotalTiles * argInfo.numTiles.at(d);
             numTotalTiles = simplify(numTotalTiles);
-            enableDivideBy(numTotalTiles, context);
 
             auto numTilesVarType = resultType(numTotalTiles).varType;
             auto one             = Expression::literal(1, numTilesVarType);
@@ -1567,8 +1566,6 @@ namespace rocRoller
                                                            numTilesDT,
                                                            DataDirection::ReadOnly,
                                                            argInfo.numTileArgExprs[d]}));
-                if(d > 0)
-                    enableDivideBy(argInfo.numTiles.back(), context);
             }
 
             argInfo.numTiles.push_back(k->addArgument({"numTilesAcc",
